dd0a940bc0b9748696f3199fb8ed8a12b5fb4885
lucianoMocchegiani <lucianomocchegiani@hotmail.com.ar>
2024-09-06 11:24:41 -0300

    updated

--- pokemon-battle-client/src/hooks/useBattle.jsx
+++ pokemon-battle-client/src/hooks/useBattle.jsx
@@ -1,44 +1,43 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { postBattle } from "../services/pokemon"
 
-export function useBattle({pokemons}){
+export function useBattle({ pokemons }) {
     const [battle, setBattle] = useState({
-        data:null,
-        success:true,
-        message:null,
+        data: null,
+        success: true,
+        message: null,
     })
     const [loading, setLoading] = useState(false)
     const [selectPokemon, setSelectPokemon] = useState(null)
     const [randomPokemon, setRandomPokemon] = useState(null)
 
-    const handleSetRandomPokemon = ()=>{
-        if(!pokemons){return}
-        const randomIndex = Math.floor(Math.random() * pokemons.length)
+    const handleSetRandomPokemon = () => {
+        if (!pokemons) { return }
+        const arrayPokemons = selectPokemon? pokemons.filter( e => e.id != selectPokemon.id): pokemons
+        const randomIndex = Math.floor(Math.random() * arrayPokemons.length)
         const newRandomPokemon = pokemons[randomIndex]
         setRandomPokemon(newRandomPokemon)
         return newRandomPokemon
     }
 
-    const handleSelectPokemon = (pokemon)=>{
+    const handleSelectPokemon = (pokemon) => {
         setSelectPokemon(pokemon)
         setRandomPokemon(null)
     }
 
-    const handleStartBattle = async ()=>{
+    const handleStartBattle = async () => {
         const newRandomPokemon = handleSetRandomPokemon()
         setLoading(true)
-        setBattle({...battle, message:null, success:true})
-        const res = await postBattle({id1:selectPokemon?.id, id2:newRandomPokemon?.id})
+        setBattle({ ...battle, message: null, success: true })
+        const res = await postBattle({ id1: selectPokemon?.id, id2: newRandomPokemon?.id })
         setBattle(res)
         setLoading(false)
-        // setSelectPokemon(null)
-        // setRandomPokemon(null)
-    } 
+    }
 
-    return{
-        result:battle.data?.result,
-        success:battle.success,
-        message:battle.message,
+    return {
+        result: battle.data?.result,
+        success: battle.success,
+        message: battle.message,
         loading,
         handleStartBattle,
         handleSelectPokemon,
